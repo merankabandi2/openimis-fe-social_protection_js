3fab8cdf5d39f3b0273506771a22ad7484f7dd9a
Anthony BELEM <anthbel@gmail.com>
2026-03-17 01:31:01 +0200

    Remove M&E code extracted to merankabandi module
    Routes, menus, refs, and imports for M&E features (dashboard,
    indicators, monetary transfers, results framework, activities,
    KoboETL) now live in openimis-fe-merankabandi_js.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 /* eslint-disable camelcase */
 /* eslint-disable import/prefer-default-export */
 import React from 'react';
-import { Tune, Dashboard, Sync, Assessment, AttachMoney, Event } from '@material-ui/icons';
+import { Tune } from '@material-ui/icons';
 import { FormattedMessage } from '@openimis/fe-core';
 import messages_en from './translations/en.json';
 import messages_fr from './translations/fr.json';
@@ -11,7 +11,6 @@ import BenefitPlanMainMenu from './menus/BenefitPlanMainMenu';
 import BenefitPlansPage from './pages/BenefitPlansPage';
 import BenefitPlanPage from './pages/BenefitPlanPage';
 import BenefitPackagePage from './pages/BenefitPackagePage';
-import KoboETLAdminPage from './pages/KoboETLAdminPage';
 import BeneficiaryStatusPicker from './pickers/BeneficiaryStatusPicker';
 import {
   BenefitPlanBeneficiariesListTabPanel,
@@ -65,79 +64,15 @@ import { fetchBenefitPlanSchemaFields } from './actions';
 import BenefitPlanHistorySearcher from './components/BenefitPlanHistorySearcher';
 import { BenefitPlanChangelogTabLabel, BenefitPlanChangelogTabPanel } from './components/BenefitPlanChangelogTab';
 import { BenefitPlanTaskTabLabel, BenefitPlanTaskTabPanel } from './components/BenefitPlanTaskTab';
-import { BenefitPlanProvincesTabLabel, BenefitPlanProvincesTabPanel } from './components/BenefitPlanProvincesTab';
 import {
   BENEFIT_PLAN_LABEL,
   RIGHT_BENEFIT_PLAN_SEARCH,
-  MONETARY_TRANSFERS_ROUTE,
-  RESULT_FRAMEWORK_ROUTE,
-  RIGHT_MONETARY_TRANSFER_SEARCH,
-  RIGHT_KOBO_ETL_VIEW,
-  RIGHT_KOBO_ETL_RUN,
-  KOBO_ETL_ROUTE,
 } from './constants';
 import BeneficiaryPicker from './pickers/BeneficiaryPicker';
-import SectionPicker from './pickers/SectionPicker';
-import { MicroProjectTabLabel, MicroProjectTabPanel } from './components/me/MicroProjectTabPanel';
-import {
-  SensitizationTrainingTabLabel,
-  SensitizationTrainingTabPanel
-} from './components/me/SensitizationTrainingTabPanel';
-import {
-  BehaviorChangePromotionTabLabel,
-  BehaviorChangePromotionTabPanel
-} from './components/me/BehaviorChangePromotionTabPanel';
-import {
-  DevelopmentIndicatorsTabLabel,
-  DevelopmentIndicatorsTabPanel
-} from './components/me/DevelopmentIndicatorsTabPanel';
-import {
-  IntermediateIndicatorsTabLabel,
-  IntermediateIndicatorsTabPanel
-} from './components/me/IntermediateIndicatorsTabPanel';
-import {
-  ResultsFrameworkTabLabel,
-  ResultsFrameworkTabPanel
-} from './components/me/ResultsFrameworkTabPanel';
-import MEMainMenu from './menus/MEMainMenu';
-import MEIndicatorsPage from './pages/MEIndicatorsPage';
-import IndicatorsPage from './pages/IndicatorsPage';
-import IndicatorPage from './pages/IndicatorPage';
-import SectionsPage from './pages/SectionsPage';
-import SectionPage from './pages/SectionPage';
-import HomePageContainer from './components/dashboard/HomePageContainer';
-import MonetaryTransferPage from './pages/MonetaryTransferPage';
-import MonetaryTransfersPage from './pages/MonetaryTransfersPage';
-import MonetaryTransfersListPage from './pages/MonetaryTransfersListPage';
-import MonetaryTransfersDashboardPage from './pages/MonetaryTransfersDashboardPage';
-import MEResultFrameworkPage from './pages/MEResultFrameworkPage';
-import MEDashboard from './components/dashboards/MEDashboard';
-import OptimizedMEDashboard from './components/dashboards/OptimizedMEDashboard';
-import ResultsFrameworkDashboard from './components/dashboard/ResultsFrameworkDashboard';
-import ActivitiesDashboard from './components/dashboard/ActivitiesDashboard';
-import ActivitiesDashboardEnhanced from './components/dashboard/ActivitiesDashboardEnhanced';
-import { useOptimizedDashboard, useDashboardSystem, useOptimizedDashboardComponent } from './hooks/useOptimizedDashboard';
 
 const ROUTE_BENEFIT_PLANS = 'benefitPlans';
 const ROUTE_BENEFIT_PLAN = 'benefitPlans/benefitPlan';
 const ROUTE_BENEFIT_PACKAGE = 'benefitPackage';
-const ROUTE_ME = 'me';
-
-const ROUTE_RESULT_FRAMEWORK = `${ROUTE_ME}/result-framework`;
-const ROUTE_MONETARY_TRANSFERS = `${ROUTE_ME}/monetary-transfers`;
-const ROUTE_MONETARY_TRANSFERS_LIST = `${ROUTE_ME}/monetary-transfers-list`;
-const ROUTE_MONETARY_TRANSFERS_DASHBOARD = `${ROUTE_ME}/monetary-transfers-dashboard`;
-const ROUTE_MONETARY_TRANSFER = `${ROUTE_ME}/monetary-transfers/monetary-transfer`;
-const ROUTE_ME_DASHBOARD = `${ROUTE_ME}/dashboard`;
-const ROUTE_RESULTS_FRAMEWORK_DASHBOARD = `${ROUTE_ME}/results-framework-dashboard`;
-const ROUTE_RESULTS_FRAMEWORK_INDICATORS_LIST = `${ROUTE_ME}/rf/indicators`;
-const ROUTE_RESULTS_FRAMEWORK_INDICATOR = `${ROUTE_ME}/rf/indicators/indicator`;
-const ROUTE_RESULTS_FRAMEWORK_SECTIONS_LIST = `${ROUTE_ME}/rf/sections`;
-const ROUTE_RESULTS_FRAMEWORK_SECTION = `${ROUTE_ME}/rf/sections/section`;
-const ROUTE_ENHANCED_RESULTS_FRAMEWORK = `${ROUTE_ME}/enhanced-results-framework`;
-const ROUTE_ACTIVITIES_DASHBOARD = `${ROUTE_ME}/activities-dashboard`;
-const ROUTE_ACTIVITIES = `${ROUTE_ME}/activities`;
-const ROUTE_KOBO_ETL_ADMIN = 'socialProtection/koboETLAdmin';
 
 const DEFAULT_CONFIG = {
   translations: [
@@ -147,9 +82,7 @@ const DEFAULT_CONFIG = {
   reducers: [{ key: 'socialProtection', reducer }],
   'core.MainMenu': [
     { name: 'BenefitPlanMainMenu', component: BenefitPlanMainMenu },
-    { name: 'MEMainMenu', component: MEMainMenu },
   ],
-  'home.HomePage.customDashboard': HomePageContainer,
   'core.Router': [
     { path: ROUTE_BENEFIT_PLANS, component: BenefitPlansPage },
     { path: `${ROUTE_BENEFIT_PLAN}/:benefit_plan_uuid?`, component: BenefitPlanPage },
@@ -161,35 +94,6 @@ const DEFAULT_CONFIG = {
       path: `${ROUTE_BENEFIT_PLAN}/:benefit_plan_uuid?/${ROUTE_BENEFIT_PACKAGE}/group/:group_beneficiaries_uuid?`,
       component: BenefitPackagePage,
     },
-    {
-      path: ROUTE_RESULTS_FRAMEWORK_INDICATORS_LIST,
-      component: IndicatorsPage,
-    },
-    {
-      path: `${ROUTE_RESULTS_FRAMEWORK_INDICATOR}/:indicator_id?`,
-      component: IndicatorPage,
-    },
-    {
-      path: ROUTE_RESULTS_FRAMEWORK_SECTIONS_LIST,
-      component: SectionsPage,
-    },
-    {
-      path: `${ROUTE_RESULTS_FRAMEWORK_SECTION}/:section_id?`,
-      component: SectionPage,
-    },
-    { path: ROUTE_MONETARY_TRANSFERS, component: MonetaryTransfersPage },
-    { path: ROUTE_MONETARY_TRANSFERS_LIST, component: MonetaryTransfersListPage },
-    { path: ROUTE_MONETARY_TRANSFERS_DASHBOARD, component: MonetaryTransfersDashboardPage },
-    { path: ROUTE_RESULT_FRAMEWORK, component: MEResultFrameworkPage },
-    { path: `${ROUTE_MONETARY_TRANSFER}/:monetary_transfer_uuid?`, component: MonetaryTransferPage },
-    { path: ROUTE_ME_DASHBOARD, component: MEDashboard },
-    { path: ROUTE_RESULTS_FRAMEWORK_DASHBOARD, component: ResultsFrameworkDashboard },
-    {
-      path: ROUTE_ACTIVITIES,
-      component: MEIndicatorsPage,
-    },
-    { path: ROUTE_ACTIVITIES_DASHBOARD, component: ActivitiesDashboard },
-    { path: ROUTE_KOBO_ETL_ADMIN, component: KoboETLAdminPage },
   ],
   refs: [
     { key: 'socialProtection.route.benefitPlan', ref: ROUTE_BENEFIT_PLAN },
@@ -204,26 +108,6 @@ const DEFAULT_CONFIG = {
     { key: 'socialProtection.fetchBenefitPlanSchemaFields', ref: fetchBenefitPlanSchemaFields },
     { key: 'socialProtection.BenefitPlanHistorySearcher', ref: BenefitPlanHistorySearcher },
     { key: 'socialProtection.BeneficiaryPicker', ref: BeneficiaryPicker },
-    { key: 'socialProtection.SectionPicker', ref: SectionPicker },
-    { key: 'socialProtection.route.monetaryTransfers', ref: ROUTE_MONETARY_TRANSFERS },
-    { key: 'socialProtection.monetaryTransfers', ref: ROUTE_MONETARY_TRANSFERS_LIST },
-    { key: 'socialProtection.route.monetaryTransfer', ref: ROUTE_MONETARY_TRANSFER },
-    { key: 'socialProtection.route.resultFramework', ref: ROUTE_RESULT_FRAMEWORK },
-    { key: 'socialProtection.route.indicators', ref: ROUTE_RESULTS_FRAMEWORK_INDICATORS_LIST },
-    { key: 'socialProtection.route.indicator', ref: ROUTE_RESULTS_FRAMEWORK_INDICATOR },
-    { key: 'socialProtection.route.sections', ref: ROUTE_RESULTS_FRAMEWORK_SECTIONS_LIST },
-    { key: 'socialProtection.route.section', ref: ROUTE_RESULTS_FRAMEWORK_SECTION },
-    { key: 'socialProtection.route.meDashboard', ref: ROUTE_ME_DASHBOARD },
-    { key: 'socialProtection.MEDashboard', ref: MEDashboard },
-    { key: 'socialProtection.route.resultsFrameworkDashboard', ref: ROUTE_RESULTS_FRAMEWORK_DASHBOARD },
-    { key: 'socialProtection.ResultsFrameworkDashboard', ref: ResultsFrameworkDashboard },
-    { key: 'socialProtection.route.activitiesDashboard', ref: ROUTE_ACTIVITIES_DASHBOARD },
-    { key: 'socialProtection.route.activities', ref: ROUTE_ACTIVITIES },
-    { key: 'socialProtection.route.koboETLAdmin', ref: ROUTE_KOBO_ETL_ADMIN },
-    { key: 'socialProtection.OptimizedMEDashboard', ref: OptimizedMEDashboard },
-    { key: 'socialProtection.useOptimizedDashboard', ref: useOptimizedDashboard },
-    { key: 'socialProtection.useDashboardSystem', ref: useDashboardSystem },
-    { key: 'socialProtection.useOptimizedDashboardComponent', ref: useOptimizedDashboardComponent },
   ],
   'benefitPlan.TabPanel.label': [
     BenefitPlanBeneficiariesListTabLabel,
@@ -231,7 +115,6 @@ const DEFAULT_CONFIG = {
     BenefitPlanBeneficiariesActiveTabLabel,
     BenefitPlanBeneficiariesSuspendedTabLabel,
     BenefitPlanTaskTabLabel,
-    BenefitPlanProvincesTabLabel,
   ],
   'benefitPlan.TabPanel.panel': [
     BenefitPlanBeneficiariesListTabPanel,
@@ -239,7 +122,6 @@ const DEFAULT_CONFIG = {
     BenefitPlanBeneficiariesActiveTabPanel,
     BenefitPlanBeneficiariesSuspendedTabPanel,
     BenefitPlanTaskTabPanel,
-    BenefitPlanProvincesTabPanel,
   ],
   'benefitPackage.TabPanel.label': [
     BenefitPackageMembersTabLabel,
@@ -250,24 +132,6 @@ const DEFAULT_CONFIG = {
     BenefitPackageMembersTabPanel,
     BenefitPackageBenefitsTabPanel,
     BenefitPackageGrievancesTabPanel,
-  ],
-  'meIndicators.TabPanel.label': [
-    MicroProjectTabLabel,
-    SensitizationTrainingTabLabel,
-    BehaviorChangePromotionTabLabel,
-  ],
-  'meIndicators.TabPanel.panel': [
-    MicroProjectTabPanel,
-    SensitizationTrainingTabPanel,
-    BehaviorChangePromotionTabPanel,
-  ],
-  'meResultFrameWork.TabPanel.label': [
-    DevelopmentIndicatorsTabLabel,
-    IntermediateIndicatorsTabLabel,
-  ],
-  'meResultFrameWork.TabPanel.panel': [
-    DevelopmentIndicatorsTabPanel,
-    IntermediateIndicatorsTabPanel,
   ],
   'tasksManagement.tasks': [{
     text: <FormattedMessage module="socialProtection" id="benefitPlan.tasks.update.title" />,
@@ -303,57 +167,6 @@ const DEFAULT_CONFIG = {
       route: '/benefitPlans',
       filter: (rights) => rights.includes(RIGHT_BENEFIT_PLAN_SEARCH),
       id: 'socialProtection.benefitPlans',
-    },
-    {
-      text: <FormattedMessage module="socialProtection" id="menu.socialProtection.koboETLAdmin" />,
-      icon: <Sync />,
-      route: `/${ROUTE_KOBO_ETL_ADMIN}`,
-      filter: (rights) => rights.includes(RIGHT_KOBO_ETL_VIEW),
-      id: 'socialProtection.koboETLAdmin',
-    },
-  ],
-  'me.MainMenu': [
-    {
-      text: <FormattedMessage module="socialProtection" id="menu.socialProtection.dashboard" />,
-      icon: <Dashboard />,
-      route: `/${ROUTE_ME_DASHBOARD}`,
-      filter: (rights) => rights.includes(RIGHT_BENEFIT_PLAN_SEARCH),
-      id: 'socialProtection.me.dashboard',
-    },
-    {
-      text: <FormattedMessage module="socialProtection" id="menu.socialProtection.activities" />,
-      icon: <Event />,
-      route: '/me/activities',
-      filter: (rights) => rights.includes(RIGHT_BENEFIT_PLAN_SEARCH),
-      id: 'socialProtection.me.activities',
-    },
-    {
-      text: <FormattedMessage module="socialProtection" id="menu.socialProtection.monetaryTransfer" />,
-      icon: <AttachMoney />,
-      route: `/${MONETARY_TRANSFERS_ROUTE}`,
-      filter: (rights) => rights.includes(RIGHT_MONETARY_TRANSFER_SEARCH),
-      id: 'socialProtection.me.monetaryTransfers',
-    },
-    {
-      text: <FormattedMessage module="socialProtection" id="menu.socialProtection.resultsFramework" />,
-      icon: <Assessment />,
-      route: `/${RESULT_FRAMEWORK_ROUTE}`,
-      filter: (rights) => rights.includes(RIGHT_MONETARY_TRANSFER_SEARCH),
-      id: 'socialProtection.me.resultsFramework',
-    },
-    {
-      text: <FormattedMessage module="socialProtection" id="menu.socialProtection.resultsFrameworkDashboard" />,
-      icon: <Dashboard />,
-      route: `/${ROUTE_RESULTS_FRAMEWORK_DASHBOARD}`,
-      filter: (rights) => rights.includes(RIGHT_MONETARY_TRANSFER_SEARCH),
-      id: 'socialProtection.me.resultsFrameworkDashboard',
-    },
-    {
-      text: <FormattedMessage module="socialProtection" id="menu.socialProtection.activitiesDashboard" />,
-      icon: <Dashboard />,
-      route: `/${ROUTE_ACTIVITIES_DASHBOARD}`,
-      filter: (rights) => rights.includes(RIGHT_MONETARY_TRANSFER_SEARCH),
-      id: 'socialProtection.me.activitiesDashboard',
     },
   ],
 };
